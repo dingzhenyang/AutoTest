9ee397c1b7757cdd8fbe265fa0e47fd99d2a4fb7
dingzhenyang <1132406641@qq.com>
2022-06-27 23:44:24 +0800

    多线程文件的修改

--- Chapter5/src/main/java/com/course/testng/multiThread/MultiThreadOnAnnotion.java
+++ Chapter5/src/main/java/com/course/testng/multiThread/MultiThreadOnAnnotion.java
@@ -6,7 +6,6 @@ public class MultiThreadOnAnnotion {
 
     @Test(invocationCount = 10, threadPoolSize = 3)
     public void test(){
-        System.out.println(1);
         System.out.printf("Thread Id : %s%n", Thread.currentThread());
     }
 }
